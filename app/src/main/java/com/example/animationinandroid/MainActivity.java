bc800b8cb8148eb20cf400bb3516a911b6543ce2
Bulbul Rathore <bulbulrathore0707@gmail.com>
2020-05-20 08:48:01 +0530

    first Animation(Rotation)

--- app/src/main/java/com/example/animationinandroid/MainActivity.java
+++ app/src/main/java/com/example/animationinandroid/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.animationinandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txthelloworld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txthelloworld = findViewById(R.id.txthelloworld);
+        txthelloworld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello World",Toast.LENGTH_LONG).show();
+                Log.i("Tag","hello world");
+                txthelloworld.animate().rotation(1000f).setDuration(1000);
+            }
+        });
     }
 }
