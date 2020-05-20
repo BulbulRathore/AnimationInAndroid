d7c452674ea79ffdc4fe397c9a74ba6ebd4bbb08
Bulbul Rathore <bulbulrathore0707@gmail.com>
2020-05-20 09:32:22 +0530

    Animation(looped animation using alpha b/w two textviews)

--- app/src/main/java/com/example/animationinandroid/MainActivity.java
+++ app/src/main/java/com/example/animationinandroid/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private TextView txthelloworld;
+    private TextView txthelloworld1;
+    private boolean isHelloworldShowing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txthelloworld = findViewById(R.id.txthelloworld);
+        txthelloworld1 = findViewById(R.id.txthelloworld1);
         txthelloworld.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Hello World",Toast.LENGTH_LONG).show();
-                Log.i("Tag","hello world");
-                txthelloworld.animate().alpha(0.5f).setDuration(1000);
+//                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+//                Log.i("Tag", "hello world");
+                if (!isHelloworldShowing){
+                    txthelloworld.animate().alpha(0f).setDuration(3000);
+                txthelloworld1.animate().alpha(1f).setDuration(3000);
+                isHelloworldShowing = true;
+               } else{
+                    txthelloworld1.animate().alpha(0f).setDuration(3000);
+                    txthelloworld.animate().alpha(1f).setDuration(3000);
+                    isHelloworldShowing = false;
+                }
             }
         });
     }
+
+
 }
